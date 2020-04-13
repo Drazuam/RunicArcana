6d1b1f7e4f65e147bb21a40eafc3f1e248e9d055
Joel Huff <drazonu@gmail.com>
2020-04-13 13:18:00 -0500

    start of 1.15 rewrite

--- src/main/java/com/drazuam/runicarcana/api/enchantment/Signals/ISignal.java
+++
@@ -1,9 +0,0 @@
-package com.drazuam.runicarcana.api.enchantment.Signals;
-
-/**
- * Created by Joel on 2/23/2017.
- */
-public interface ISignal {
-    Object doFunction(Object... args);
-
-}
--- src/main/java/com/drazuam/runicarcana/common/event/IEventHandler.java
+++
@@ -1,7 +0,0 @@
-package com.drazuam.runicarcana.common.event;
-
-/**
- * Created by Joel on 2/21/2017.
- */
-public interface IEventHandler {
-}
