98654e25c4edccf5e7eba38e6dd92b753d39fac6
Alleluid <me@alleluid.com>
2020-04-21 00:53:48 -0700

    Add Principium assets

---
+++ src/main/java/com/latenighters/runicarcana/common/blocks/PrincipicBlock.java
@@ -0,0 +1,15 @@
+package com.latenighters.runicarcana.common.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+public class PrincipicBlock extends Block {
+    public PrincipicBlock() {
+        super(Block.Properties.create(Material.GLASS)
+                .sound(SoundType.METAL)
+                .hardnessAndResistance(1.5f)
+                .lightValue(14)
+        );
+    }
+}
